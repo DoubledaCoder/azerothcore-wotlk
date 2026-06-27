0d8040d9daf79824de7044a40f2177cca760378f
Justin <224400640+DoubledaCoder@users.noreply.github.com>
2026-06-27 16:44:47 +0800

    Update Unit.cpp
    Updated as per sudlud's suggestion to move the hook to the actual point where combat ends so it follows the actual combat exit pathway.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7590,6 +7590,7 @@ void Unit::UpdatePetCombatState()
 void Unit::AtExitCombat()
 {
     RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_LEAVE_COMBAT);
+    sScriptMgr->OnUnitStopCombat(this);
 }
 
 void Unit::AtEngage(Unit* /*target*/)
